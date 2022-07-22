c683f9caadab7dfcdef9d2721d9808f4854279c6
Blue Jessen <canealisblue@gmail.com>
2022-07-21 21:59:58 -0700

    Create skeleton for testing points

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,5 +1,32 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Dashboard', () => {
+  it('Should the main logo', () => {
     cy.visit('https://example.cypress.io')
+  })
+
+  it('Should have three tabs in its appointment block', () => {
+
+  })
+
+  it('Should be able to click on one of the service options and have it change to the form tab', () => {
+
+  })
+  it('Should be able to go back to the initial tab and change services', () => {
+
+  })
+
+  it('Should have a different form based on the service clicked by user', () => {
+
+  })
+
+  it('Should auto fill to the consult/contact form if the user does not pick a service option', () => {
+
+  })
+
+  it('Should be able to fill out form', () => {
+
+  })
+
+  it('Should switch to the confirmation tab when they hit submit on the form', () => {
+
   })
 })

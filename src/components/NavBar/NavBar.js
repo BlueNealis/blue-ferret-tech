3e80f140ae8ff4a35267df939c9c764e8e88343d
Blue Jessen <canealisblue@gmail.com>
2022-07-25 14:06:14 -0700

    Fix text styling in nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,13 @@ import './NavBar.css'
 
 const NavBar = () => {
   return (<header className='nav-bar'>
-    <h1>Ferret Customs & Tech</h1>
+    <div className='nav-element'>
+      <h1 className='logo-text'>Ferret Customs & Tech</h1>
+    </div>
+    <div className='nav-element'>
+      <h1>Menu</h1>
+      <img></img> 
+    </div>
     </header>
   )
 }

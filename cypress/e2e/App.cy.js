ab0ecc1c35b4739af74155e334f3c0ae54efde78
Blue Jessen <canealisblue@gmail.com>
2022-07-25 19:06:37 -0700

    Add return to service selection test

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -13,25 +13,28 @@ describe('Dashboard', () => {
   })
 
   it('Should be able to click on one of the service options and have it change to the form tab', () => {
-    cy.get('.appt-block').within(()=> {
-      cy.get('appt-card').eq(0).click()
-      cy.get('form').should('have.id', 'hardware-form')
-  })
-    cy.visit('localhost:3000')
-    cy.get('.appt-block').within(()=> {
-      cy.get('appt-card').eq(1).click()
-      cy.get('form').should('have.id', 'software-form')
-  })
-
-    cy.visit('localhost:3000')
-    cy.get('.appt-block').within(()=> {
-      cy.get('appt-card').eq(2).click()
-      cy.get('form').should('have.id', 'building-form')
-  })
+      cy.get('.appt-block').within(()=> {
+        cy.get('appt-card').eq(0).click()
+        cy.get('form').should('have.id', 'hardware-form')
+    })
+      cy.visit('localhost:3000')
+      cy.get('.appt-block').within(()=> {
+        cy.get('appt-card').eq(1).click()
+        cy.get('form').should('have.id', 'software-form')
+    })
+      cy.visit('localhost:3000')
+      cy.get('.appt-block').within(()=> {
+        cy.get('appt-card').eq(2).click()
+        cy.get('form').should('have.id', 'building-form')
+    })
 })
 
   it('Should be able to go back to the initial tab and change services', () => {
-
+    cy.get('.appt-block').within(() => {
+      cy.get('appt-card').eq(2).click()
+      cy.get('form-nav').find('Select Service').click()
+      cy.get('appt-card').should('have.length', 3)
+    })
   })
 
   it('Should have a different form based on the service clicked by user', () => {

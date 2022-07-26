7dca140d30d0a2fa8a3e4dc9453d94f5f560378a
Blue Jessen <canealisblue@gmail.com>
2022-07-25 19:03:23 -0700

    Add appt click testing

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,16 +1,35 @@
 describe('Dashboard', () => {
-  it('Should the main logo', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach('Visit', () => {
+    cy.visit('localhost:3000')
   })
 
   it('Should have three tabs in its appointment block', () => {
-
+    cy.get('.appt-block').within(()=> {
+      cy.get('appt-card').should('have.length', 3)
+      cy.get('appt-card').eq(0).contains('Hardware')
+      cy.get('appt-card').eq(1).contains('Software')
+      cy.get('appt-card').eq(2).contains('Building')
+    })
   })
 
   it('Should be able to click on one of the service options and have it change to the form tab', () => {
-
+    cy.get('.appt-block').within(()=> {
+      cy.get('appt-card').eq(0).click()
+      cy.get('form').should('have.id', 'hardware-form')
   })
-  
+    cy.visit('localhost:3000')
+    cy.get('.appt-block').within(()=> {
+      cy.get('appt-card').eq(1).click()
+      cy.get('form').should('have.id', 'software-form')
+  })
+
+    cy.visit('localhost:3000')
+    cy.get('.appt-block').within(()=> {
+      cy.get('appt-card').eq(2).click()
+      cy.get('form').should('have.id', 'building-form')
+  })
+})
+
   it('Should be able to go back to the initial tab and change services', () => {
 
   })

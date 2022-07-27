929348cd3d0618a6e3f9bd8ac98aeccc6401ec2f
Blue Jessen <canealisblue@gmail.com>
2022-07-26 22:05:51 -0700

    Add Checkbox component and base form

---
+++ src/components/Form/CustomForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CustomForm = ({type}) => {
+
+
+  return(<>
+    <checkbox></checkbox>
+    <checkbox></checkbox>
+    </>
+  )
+}
+
+export default CustomForm
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CustomForm from './CustomForm'
-const Form = () => {
+import Checkbox from './Checkbox'
 
+const Form = ({type}) => {
+const [checked, setChecked] = React.useState(false)
+
+const handleChange = () => {
+  setChecked(!checked)
+}
   return (
     <form>
       <input type='text' placeholder='Name: First Last'></input>
+        <br/>
       <input type='text' placeholder='email@example.com'></input>
+        <br/>
       <input type='number' placeholder='803-000-0000'></input>
-      <checkbox></checkbox>
-      <checkbox></checkbox>
-      <CustomForm />
-      <checkbox></checkbox>
+        <br/>
+      <Checkbox label='Mobile' checked={checked} onChange={handleChange}/>
+      <Checkbox label='Home' checked={checked} onChange={handleChange}/>
+        <br/>
+      <CustomForm type={type}/>
+        <br/>
+      <Checkbox label='Desktop' checked={checked} onChange={handleChange}/>
+      <Checkbox label='Laptop' checked={checked} onChange={handleChange}/>
+        <br/>
+      <input type='text' Placeholder='Other enter'></input>
+        <br/>
       <input type='text' placeholder='Any additional details I should know?'></input>
+      <button>Submit</button> 
     </form>
   )
 }

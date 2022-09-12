086d8c9a7d4bdca572af56ed3311734f5760dfe0
Blue Jessen <canealisblue@gmail.com>
2022-09-11 18:07:09 -0700

    Add state that makes a white box visible on menu click

--- src/components/MenuBurger/MenuBurger.js
+++ src/components/MenuBurger/MenuBurger.js
@@ -2,7 +2,11 @@ import React, {useState} from 'react'
 import './MenuBurger.css'
 
 function MenuBurger() {
-  return()
+  const [isVisible, setIsVisible] = useState(false)
+  return(
+    <div className='menu-box'>
+    </div>
+  )
 }
 
 export default MenuBurger
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import MenuBurger from '../MenuBurger/MenuBurger'
 import './NavBar.css'
 
 const NavBar = ({resetForm}) => {
+    const [isMenuVisible, setIsMenuVisible] = useState(false)
+
   return (<header className='nav-bar'>
     <div className='nav-element'>
       <h1 onClick={resetForm} className='logo-text'>Ferret Customs & Tech</h1>
     </div>
     <div className='nav-element'>
-      <h1 className='menu'>Menu</h1>
-      <img></img>
+      <h1 className='menu' onClick={(e) => setIsMenuVisible(!isMenuVisible)}>Menu</h1>
+    {isMenuVisible && <MenuBurger />}
     </div>
     </header>
   )

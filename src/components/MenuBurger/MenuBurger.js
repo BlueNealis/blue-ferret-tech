fb8f1e321ed216bb650003cf864b8ec3f07b64e2
Blue Jessen <canealisblue@gmail.com>
2022-09-11 18:30:23 -0700

    Add button sections that highlight when hovered over

--- src/components/MenuBurger/MenuBurger.js
+++ src/components/MenuBurger/MenuBurger.js
@@ -5,6 +5,15 @@ function MenuBurger() {
   const [isVisible, setIsVisible] = useState(false)
   return(
     <div className='menu-box'>
+      <div className='menu-section'>
+        <h1>Shop My Builds</h1>
+      </div>
+      <div className='menu-section'>
+        <h1>About Me</h1>
+      </div>
+      <div className='menu-section'>
+        <h1>View Cart</h1>
+      </div>
     </div>
   )
 }

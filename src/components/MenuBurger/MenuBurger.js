4507109a332fe4ce234a2d8ffd33b1fef28e4410
Blue Jessen <canealisblue@gmail.com>
2022-09-11 18:34:35 -0700

    Add links to other pages for menu

--- src/components/MenuBurger/MenuBurger.js
+++ src/components/MenuBurger/MenuBurger.js
@@ -1,19 +1,26 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './MenuBurger.css'
 
 function MenuBurger() {
   const [isVisible, setIsVisible] = useState(false)
   return(
     <div className='menu-box'>
+    <Link to='/builds'>
       <div className='menu-section'>
         <h1>Shop My Builds</h1>
       </div>
+    <Link/>
+    <Link to='/AboutMe'>
       <div className='menu-section'>
         <h1>About Me</h1>
       </div>
+    </Link>
+    <Link to='MyCart'>
       <div className='menu-section'>
         <h1>View Cart</h1>
       </div>
+    </Link>
     </div>
   )
 }

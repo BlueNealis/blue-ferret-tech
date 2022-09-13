65f3b2453c542df44b835f1b8ac113d40adf8bb3
Blue Jessen <canealisblue@gmail.com>
2022-09-12 20:15:52 -0700

    Add a builds page file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useState } from 'react'
 import NavBar from './components/NavBar/NavBar.js'
 import AptWindow from './components/AppointmentWindow/AptWindow.js'
 import Form from './components/Form/Form'
-import {Link, Route} from 'react-router-dom'
+import {Link, Route, Switch} from 'react-router-dom'
+import BuildsPage from './components/BuildsPage/BuildsPage'
 
 const App = () => {
   const [formType, setFormType] = useState('')
@@ -19,15 +20,20 @@ const App = () => {
 
   return (
     <div className="App">
-      <NavBar resetForm={resetForm}/>
-      {!formType && <AptWindow handleClick={handleClick}/>}
-      {formType && <Form type={formType} />}
-      <footer>
-        <h1 style={{fontSize:'.5rem'}}>Background Photo by
-        <a href="https://unsplash.com/es/@fellowshipbattlecreek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ron Kenney</a> on
-        <a href="https://unsplash.com/s/photos/south-carolina?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-        </h1>
-      </footer>
+      <Route path='/'>
+        <NavBar resetForm={resetForm}/>
+        {!formType && <AptWindow handleClick={handleClick}/>}
+        {formType && <Form type={formType} />}
+        <footer>
+          <h1 style={{fontSize:'.5rem'}}>Background Photo by
+          <a href="https://unsplash.com/es/@fellowshipbattlecreek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ron Kenney</a> on
+          <a href="https://unsplash.com/s/photos/south-carolina?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+          </h1>
+        </footer>
+      </Route>
+      <Route path='/builds'>
+        <BuildsPage />
+      </Route>
     </div>
   );
 }

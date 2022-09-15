96f59aa81ca3bc8705b2bfd3c10b7b8c9859ce8c
Blue Jessen <canealisblue@gmail.com>
2022-09-14 21:20:10 -0700

    Add logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react'
 import MenuBurger from '../MenuBurger/MenuBurger'
 import './NavBar.css'
+import mainLogo from './ferret.png'
 
 const NavBar = ({resetForm}) => {
     const [isMenuVisible, setIsMenuVisible] = useState(false)
 
   return (<header className='nav-bar'>
+  <img src={mainLogo} className='ferretLogo' alt='ferret logo'/>
     <div className='nav-element'>
       <h1 onClick={resetForm} className='logo-text'>Ferret Customs & Tech</h1>
     </div>

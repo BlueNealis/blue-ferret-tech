5bb347feaa192a1e2197d49d1fb15bf03c02fd32
Blue Jessen <canealisblue@gmail.com>
2022-10-06 23:14:38 -0700

    Add more spacing to detail page

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
+import products from '../productsData.js'
+import './ProductDetail.css'
 
 function ProductDetail({id}) {
+  const data = products.find((product) => {
+    return product.id === parseInt(id)
+  })
+  const details = data.specs.map((detail) => {
+    return <p className='bullet-point'> - {detail}</p>
+  })
+
   return(
-    <h1>{`${id}`}</h1>
+    <>
+      <NavBar />
+      <div className='product-page-wrapper'>
+        <div className='detail-container'>
+          <img src={data.images[0]}/>
+          <div className='details'>
+          <h1 className='detail-title'>{data.name}</h1>
+          {details}
+          <h1 className='detail-price'>{data.price}</h1>
+          </div>
+        <button className='add-cart-button'> Add To Cart </button>
+        </div>
+      </div>
+    </>
   )
 }
 

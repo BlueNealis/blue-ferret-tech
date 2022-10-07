e20c62c3527edca1c7c82d8cc6bc01e13a670a87
Blue Jessen <canealisblue@gmail.com>
2022-10-06 19:01:09 -0700

    Set up base html for product card

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './ProductCard.css'
 import products from '../productsData'
+import { Link } from 'react-router-dom'
 
-function ProductCard() {
+function ProductCard({title, price, imgUrl}) {
+  let key = 5654;
   return(
-
+  <Link to={`/${title}/${key}`}>
+    <div className='product-card'>
+      <h1>{title}</h1>
+      <img src={imgUrl}/>
+      <p>{price}</p>
+    </div>
+  </Link>
   )
 }
 

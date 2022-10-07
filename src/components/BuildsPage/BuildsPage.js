32224bc385d5f46776be43a84923010b130bc281
Blue Jessen <canealisblue@gmail.com>
2022-10-06 17:52:45 -0700

    Add products file for dummy data

--- src/components/BuildsPage/BuildsPage.js
+++ src/components/BuildsPage/BuildsPage.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import NavBar from '../NavBar/NavBar'
 
 function BuildsPage(){
   return(
     <>
       <NavBar />
-      <h1>BuildPage</h1>
+      <div classname='builds-container'>
+      </div>
     </>
   )
 }
---
+++ src/productsData.js
@@ -0,0 +1,8 @@
+const products = [
+  {name:, images:[], buildDate:, specs:, status:, price:, sale:, saleAmount:,},
+  {name:, images:[], buildDate:, specs:, status:, price:, sale:, saleAmount:,},
+  {name:, images:[], buildDate:, specs:, status:, price:, sale:, saleAmount:,},
+  {name:, images:[], buildDate:, specs:, status:, price:, sale:, saleAmount:,},
+  {name:, images:[], buildDate:, specs:, status:, price:, sale:, saleAmount:,},
+  {name:, images:[], buildDate:, specs:, status:, price:, sale:, saleAmount:,}
+]
